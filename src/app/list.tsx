ef766eedbf607b426f0909e460df43703d560633
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-16 00:06:16 +0600

    added search feature

--- src/app/list.tsx
+++ src/app/list.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { tools } from "@/db/tools";
 import { TTool } from "@/types/tool";
 import { Search } from "lucide-react";
+import { useState } from "react";
 
 type Props = {
   openTool: (tool: TTool) => void;
@@ -11,6 +12,13 @@ type Props = {
 
 const ListSection = (props: Props) => {
   const { openTool, selectedTool } = props;
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredTools = tools.filter((tool) =>
+    tool.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div
       className={`transition-transform duration-300 ease-in-out overflow-hidden ${
@@ -19,19 +27,18 @@ const ListSection = (props: Props) => {
     >
       <div className="relative mb-4">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
-        <Input placeholder="Search tools..." className="pl-10" />
+        <Input
+          placeholder="Search tools..."
+          className="pl-10"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
 
       <div className="space-y-3">
-        {tools.map((tool: TTool) => {
-          return (
-            <ToolCard
-              key={tool.key}
-              data={tool}
-              onClick={() => openTool(tool)}
-            />
-          );
-        })}
+        {filteredTools.map((tool: TTool) => (
+          <ToolCard key={tool.key} data={tool} onClick={() => openTool(tool)} />
+        ))}
       </div>
     </div>
   );
