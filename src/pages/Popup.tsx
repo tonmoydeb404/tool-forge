a6e03a405df8333e82ab77f985ca40a30f96c3bd
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-15 18:05:00 +0600

    Initiated project

---
+++ src/pages/Popup.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const Popup = (props: Props) => {
+  return <div className="">Popup</div>;
+};
+
+export default Popup;
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    webExtension({
+      manifest: generateManifest,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
